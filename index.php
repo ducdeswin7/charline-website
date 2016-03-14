c3964eeb4be52d7619a6e6b1149dace9a7bb1039
phrenel lawson <phrenel lawson>
2016-03-14 19:19:53 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,36 @@
+<!doctype html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Portfolio | Charline Miranda</title>
+    <meta name="description" content="Portfolio en ligne de Charline Miranda, designer de l'interaction et directeur artistique"/>
+    <meta name="keywords" content="design, graphisme, charline, miranda, interaction, direction artistique, art direction"/>
+    <meta name="author" content="Charline Miranda"/>
+
+    <link rel="shortcut icon" href="assets/images/favicon.png">
+    <link rel="stylesheet" href="dist/css/main.css">
+</head>
+<body>
+    <div class="container">
+        <!-- landing page -->
+        <section class="landing">
+            <div class="landing_opacity"></div> <!-- Opacity color -->
+            <div class="landing_content">
+                <div class="landing_content_logo text-center">
+                    <img class="landing_content_logo_image" src="assets/images/logo.png">
+                </div>
+                <h1 class="landing_content_title text-center">Charline Miranda</h1>
+                <div class="landing_content_line"></div>
+                <p class="landing_content_text text-center">Designer de l'interaction</p>
+                <div class="landing_content_link text-center">
+                    <a class="landing_content_link_portfolio" href="#portfolio">Portfolio</a>
+                </div>
+                <div class="arrow-down"></div>
+            </div>
+        </section>
+    </div>
+<script src="dist/javascripts/scripts.js"></script>
+</body>
+</html>
