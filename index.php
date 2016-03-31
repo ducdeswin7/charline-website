48ea8e1f8ed83d93a62938dfb29046ff5d680702
Boevi L <ducdeswin@outlook.com>
2016-03-31 23:40:23 +0200

    workin on contact us

--- index.php
+++ index.php
@@ -18,14 +18,16 @@
         <section class="landing">
             <div class="landing_opacity"></div> <!-- Opacity color -->
             <div class="landing_content">
-                <div class="landing_content_logo text-center">
-                    <img class="landing_content_logo_image" src="assets/images/logo.png">
-                </div>
-                <h1 class="landing_content_title text-center">Charline Miranda</h1>
-                <div class="landing_content_line"></div>
-                <p class="landing_content_text text-center">Designer de l'interaction</p>
-                <div class="landing_content_link text-center">
-                    <a class="landing_content_link_portfolio" href="#portfolio">Portfolio</a>
+                <div class="landing_content_center">
+                    <div class="landing_content_center_logo text-center">
+                        <img class="landing_content_center_logo_image" src="assets/images/logo.png">
+                    </div>
+                    <h1 class="landing_content_center_title text-center">Charline Miranda</h1>
+                    <div class="landing_content_center_line"></div>
+                    <p class="landing_content_center_text text-center">Designer de l'interaction</p>
+                    <div class="landing_content_center_link text-center">
+                        <a class="landing_content_center_link_portfolio" href="#portfolio">Portfolio</a>
+                    </div>
                 </div>
                 <div class="landing_content_arrow-down"></div>
             </div>
@@ -54,18 +56,188 @@
             <hr class="portfolio_line">
             <div class="portfolio_content">
                 <div class="portfolio_content_row">
-                    <div class="portfolio_content_row_item">
-                        <img src="assets/images/portfolio/1.png" alt="" class="portfolio_content_row_item_img">
-                        <div class="portfolio_content_row_item_full">
-                            <div class="portfolio_content_row_item_full_text">
-                                <h2 class="portfolio_content_row_item_full_text_title">From sound to life</h2>
-                                <p class="portfolio_content_row_item_full_text_paragraph">Installation</p>
+                    <div class="hoverme">
+                        <a href="">
+                            <img class="hoverme_img" src="assets/images/portfolio/1.png">
+                            <div class="hoverme_content">
+                                <div class="hoverme_content_text">
+                                    <h2 class="hoverme_content_text_title">From sound to life</h2>
+                                    <p class="hoverme_content_text_subtext">installation</p>
+                                </div>
                             </div>
-                        </div>
+                        </a>
+                    </div>
+                    <div class="hoverme">
+                        <a href="">
+                            <img class="hoverme_img" src="assets/images/portfolio/2.png">
+                            <div class="hoverme_content">
+                                <div class="hoverme_content_text">
+                                    <h2 class="hoverme_content_text_title">Day dreamer</h2>
+                                    <p class="hoverme_content_text_subtext">s&eacute;rie d'autoportraits</p>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                    <div class="hoverme">
+                        <a href="">
+                            <img class="hoverme_img" src="assets/images/portfolio/3.png">
+                            <div class="hoverme_content">
+                                <div class="hoverme_content_text">
+                                    <h2 class="hoverme_content_text_title">Drive faster</h2>
+                                    <p class="hoverme_content_text_subtext">aquarelles</p>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                </div>
+
+                <div class="portfolio_content_row">
+                    <div class="hoverme">
+                        <a href="">
+                            <img class="hoverme_img" src="assets/images/portfolio/4.png">
+                            <div class="hoverme_content">
+                                <div class="hoverme_content_text">
+                                    <h2 class="hoverme_content_title">Libra</h2>
+                                    <p class="hoverme_content_text_subtext">identit&eacute; visuelle & design produit</p>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                    <div class="hoverme">
+                        <a href="">
+                            <img class="hoverme_img" src="assets/images/portfolio/5.png">
+                            <div class="hoverme_content">
+                                <div class="hoverme_content_text">
+                                    <h2 class="hoverme_content_text_title">Polaris</h2>
+                                    <p class="hoverme_content_text_subtext">identit&eacute; visuelle & design produit</p>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                    <div class="hoverme">
+                        <a href="">
+                            <img class="hoverme_img" src="assets/images/portfolio/6.png">
+                            <div class="hoverme_content">
+                                <div class="hoverme_content_text">
+                                    <h2 class="hoverme_content_text_title">Spotify</h2>
+                                    <p class="hoverme_content_text_subtext">interfaces</p>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                </div>
+
+                <div class="portfolio_content_row">
+                    <div class="hoverme">
+                        <a href="">
+                            <img class="hoverme_img" src="assets/images/portfolio/7.png">
+                            <div class="hoverme_content">
+                                <div class="hoverme_content_text">
+                                    <h2 class="hoverme_content_text_title">Bright light</h2>
+                                    <p class="hoverme_content_text_subtext">lightpainting</p>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                    <div class="hoverme">
+                        <a href="">
+                            <img class="hoverme_img" src="assets/images/portfolio/8.png">
+                            <div class="hoverme_content">
+                                <div class="hoverme_content_text">
+                                    <h2 class="hoverme_content_text_title">Shoe me</h2>
+                                    <p class="hoverme_content_text_subtext">illustrations vectorielles</p>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                    <div class="hoverme">
+                        <a href="">
+                            <img class="hoverme_img" src="assets/images/portfolio/9.png">
+                            <div class="hoverme_content">
+                                <div class="hoverme_content_text">
+                                    <h2 class="hoverme_content_text_title">Universe</h2>
+                                    <p class="hoverme_content_text_subtext">objet connect&eacute;</p>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                    <div class="hoverme">
+                        <a href="">
+                            <img class="hoverme_img" src="assets/images/portfolio/11.png">
+                            <div class="hoverme_content">
+                                <div class="hoverme_content_text">
+                                    <h2 class="hoverme_content_text_title">Affiches</h2>
+                                    <p class="hoverme_content_text_subtext">Set d'affiches</p>
+                                </div>
+                            </div>
+                        </a>
                     </div>
                 </div>
             </div>
         </div>
+
+        <section class="about-us" id="about-us">
+            <h1 class="about-us_title">&Agrave; PROPOS</h1>
+            <hr class="about-us_line"/>
+            <!-- <p class="title">Passion&eacute;e de design</p> -->
+
+            <div class="about-us_content">
+                <div class="about-us_content_item">
+
+                    <div class="about-us_content_item_circle">
+                        <img src="assets/images/icons/formation.png" alt="">
+                    </div>
+
+                    <h2 class="about-us_content_item_title">FORMATION</h2>
+                    <ul class="about-us_content_item_list">
+                        <li class="about-us_content_item_list_item">Depuis 2014 | design de l'interaction, <br> e-artsup Paris</li>
+                        <li class="about-us_content_item_list_item">Depuis 2011 | e-artsup Paris</li>
+                        <li class="about-us_content_item_list_item">Depuis 2011 | baccalauréat série S SVT</li>
+                    </ul>
+
+                </div>
+
+                <div class="about-us_content_item">
+                    <div class="about-us_content_item_circle">
+                        <img src="assets/images/icons/experience.png" alt="">
+                    </div>
+                    <h2 class="about-us_content_item_title">EXP&Eacute;RIENCES</h2>
+                    <ul class="about-us_content_item_list">
+                        <li class="about-us_content_item_list_item">2015 | exposition lors du festival Exit, <br>à la Maison des Arts de Créteil</li>
+                        <li class="about-us_content_item_list_item">2014 | stage de 4 mois<br> assistante DA, agence Australie</li>
+                        <li class="about-us_content_item_list_item">014 | stage de 6 mois<br> assistance DA, agence Marquise Contents</li>
+                    </ul>
+                </div>
+
+                <div class="about-us_content_item">
+                    <div class="about-us_content_item_circle">
+                        <img src="assets/images/icons/tool.png" alt="">
+                    </div>
+                    <h2 class="about-us_content_item_title">COMP&Eacute;TENCES</h2>
+                    <ul class="about-us_content_item_list">
+                        <li class="about-us_content_item_list_item">Suite Adobe</li>
+                        <li class="about-us_content_item_list_item">Arduino</li>
+                        <li class="about-us_content_item_list_item">Processing</li>
+                        <li class="about-us_content_item_list_item">Dessin traditionnel</li>
+                        <li class="about-us_content_item_list_item">Photographie</li>
+                    </ul>
+                </div>
+            </div>
+            <div class="about-us_cv">
+                <a class="about-us_cv_link" href="assets/images/charline_miranda_cv.pdf" target="_blank">Télécharger mon CV</a>
+            </div>
+        </section>
+
+        <section class="contact">
+            <h1 class="contact_title">CONTACT</h1>
+            <hr class="contact_line"/>
+
+            <div class="contact_content">
+
+                
+            </div>
+        </section>
+
     </div>
 <script src="assets/javascripts/jquery-2.2.2.min.js"></script>
 <script src="dist/javascripts/scripts.js"></script>
